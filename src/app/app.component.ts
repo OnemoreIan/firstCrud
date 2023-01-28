88a9a7dd37726bacdb294927f9bcc6dccfceb169
ian <iandelrazo@gmail.com>
2023-01-28 17:43:41 -0600

    Last update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,29 +8,47 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title:string = 'Angular crud';
   employes = [
-    {name: 'fatz', position: 'manager',mail: 'correo de ejemplo'},
-    {name: 'Lupe', position: 'disigner',mail: 'correo de ejemplo'},
-    {name: 'Mark', position: 'programmer',mail: 'correo de ejemplo'}
+    {name: 'fatz', position: 'manager',email: 'correo de fatz'},
+    {name: 'Lupe', position: 'disigner',email: 'correo de Lupe'},
+    {name: 'Mark', position: 'programmer',email: 'correo de Mark'}
   ]
 
   model:any ={};
   model2:any = {};
+  myvalue!:any;
+  msg:string = 'Ejemplo';
 
   addEmploy():void{
     alert('Dato almacenado exitosamente');
     this.employes.push(this.model)
   }
 
-  delEmploy():void{
+  delEmploy(i:number):void{
     //this.employes.pop()
+    let confrimacion = confirm('Are you shure to continue this acction?');
+    if (confrimacion) {
+      this.employes.splice(i, 1)
+    }
   }
 
-  editEmploy():void{
-
+  editEmploy(i:number):void{
+    this.model2.name = this.employes[i].name;
+    this.model2.position = this.employes[i].position;
+    this.model2.email = this.employes[i].email;
+    this.myvalue = i;
   }
 
   updateEmploy():void{
-
+    //alert('datos recompilados');
+    let i = this.myvalue;
+    for (let j = 0; j < this.employes.length; j++) {
+      if (i == j) {
+        this.employes[i] = this.model2;
+        this.model2 = {};
+      }
+      
+    }
+    console.log(this.model2);
   }
 
 }
